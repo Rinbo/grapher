6fda58d07ea4c7fee6ce74b5769128a3fd395eb2
Rinbo <robin.b@outlook.com>
2019-05-11 20:44:52 +0200

    Experiment with adding fields

--- src/components/GraphContainer.js
+++ src/components/GraphContainer.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GraphContainer = () => {
+  const [inputsCount, setInputCount] = useState([null, null, null]);
+
+  const renderInputs = () => {
+    return inputsCount.map((e, index) => {
+      return (
+        <div className="ui input" style={{ marginTop: 5, display: "block" }}>
+          <input key={index} value={e} placeholder="0" />
+        </div>
+      );
+    });
+  };
+
   return (
     <div className="ui container">
       <div className="ui centered header" style={{ paddingTop: 30 }}>
         Grapher
+      </div>
+      <div style={{ marginTop: 20 }}>{renderInputs()}</div>
+      <div style={{ marginTop: 10 }}>
+        <button
+          className="ui basic tiny black button"
+          onClick={() => setInputCount(prevState => [...prevState, null])}
+        >
+          +
+        </button>
       </div>
     </div>
   );
