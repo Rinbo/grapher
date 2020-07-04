95b6dd959fcbff602601a432840bf250cb2d989c
Rinbo <robin.b@outlook.com>
2020-07-04 11:09:55 +0200

    Change backend dns

--- src/apis/endpoint.js
+++ src/apis/endpoint.js
@@ -4,7 +4,7 @@ let backendHost;
 const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "grapher.borjessons.nu") {
-  backendHost = "https://typing2.borjessons.nu/g-end";
+  backendHost = "https://api.borjessons.nu/g-end";
 } else {
   backendHost = "http://localhost:8080/g-end";
 }
